5b4dffe42695c1489533e5bdca6108b6906c90c7
adrianaska0 <adrian.aska004@gmail.com>
2024-12-17 04:09:31 -0500

    added minio utils

---
+++ app/utils/minio.py
@@ -0,0 +1,37 @@
+from minio import minio
+import os
+import logging
+
+def getClient ():
+    client = Minio(
+        os.getenv("MINIO_URL"),
+        access_key=os.getenv("MINIO_ACCESS_KEY"),
+        secret_key=os.getenv("MINIO_SECRET_KEY"),
+        secure=False,
+    )
+    return client
+
+def createBucket (client):
+    found = client.bucket_exists("profiles")
+    if not found:
+        client.make_bucker("profiles")
+        logging.info("Bucket 'profiles' created.")
+    else:
+        logging.info("Bucket 'profiles' already exists.")
+
+def uploadImage (client, image_path, image_name):
+    try:
+        createBucket(client)
+
+        with open(image_path, 'rb') as image_file:
+            client.fput_object(
+                "profiles",
+                image_name,
+                image_file,
+            )
+        logging.info(f"Image '{image_name}' uploaded successfully to bucket 'profiles'.")
+    except S3Error as e:
+        logging.error(f"Error uploading image '{image_name}': {e}")
+    except Exception as e:
+        logging.error(f"Unexpected error occured: {e}")
+
